915c1309d3ba32906b84d1b2e1f99d6762af03c2
Kuba Orlik <groovy354@gmail.com>
2015-04-26 16:44:17 +0200

    added various test cases for reference api

--- main.js
+++ main.js
@@ -11,11 +11,20 @@ var app = new Sealious.App(path.resolve(__dirname, "./package.json"), mode, laye
 
 var www_server = app.ChipManager.get_chip("channel", "www_server");
 
+var firma = new Sealious.ChipTypes.ResourceType("firma");
+
+firma.add_fields([
+	{name: "nazwa", type: "text", required: "true", params:{max_length:6}},
+	{name: "logo", type: "text", required: "true"}
+]);
+
+
 var stoisko = new Sealious.ChipTypes.ResourceType("stoisko");
 
 stoisko.add_fields([
 	{name: "nazwa", type: "text", required: "true"},
-	{name: "kolor", type: "color", required: "true"}
+	{name: "kolor", type: "color", required: "true"},
+	{name: "firma", type: "reference", required: false, params:{allowed_types:["firma"]}}
 ]);
 
 var form_entry = new Sealious.ChipTypes.ResourceType("form_entry");	
@@ -25,7 +34,7 @@ form_entry.add_fields([
 	{name: "last-name", type: "text", required: true},
 	{name: "PESEL", type: "text", required: true},
 	{name: "favorite-color", type: "color"},
-	{name: "stoisko", type: "reference", params:{allowed_types:["stoisko"]}}
+	{name: "stoisko", type: "reference", required: true, params:{allowed_types:["stoisko"]}}
 ])
 
 /*
@@ -39,6 +48,8 @@ form_entry.add_references([
 var rest = app.ChipManager.get_chip("channel", "rest");
 
 rest.add_path("/api/v1/form_entry", "form_entry");
+rest.add_path("/api/v1/stoiska", "stoisko");
+rest.add_path("/api/v1/firmy", "firma");
 
 www_server.static_route(path.resolve( __dirname, "./public"), "");
 
